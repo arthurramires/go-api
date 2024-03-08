cb67c6dc61f02a1730d70da02dc55bfbc30c5279
arthurramires <arthurramires1802@outlook.com>
2024-03-08 10:17:01 -0400

    feat: Added validation error and send to database

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -3,11 +3,34 @@ package handler
 import (
 	"net/http"
 
+	"github.com/arthurramires/go-api/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{
-		"msg": "POST Opening",
-	})
+	request := CreateOpeningRequest{}
+	context.BindJSON(&request)
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+		sendError(context, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("error create opening: %v", err.Error())
+		sendError(context, http.StatusInternalServerError, "error creating opening on database")
+		return
+	}
+
+	sendSuccess(context, "create opening", opening)
 }
